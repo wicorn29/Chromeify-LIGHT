82f7eaaeaa4d7a374eae71ed8d945bc809cdb02a
Wilson Corn <wilson.corn@hotmail.com>
2023-01-07 15:35:25 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
--- bash <(curl -s https://raw.githubusercontent.com/wicorn29/Chromeify-LIGHT/main/test.sh)
+echo bash <(curl -s https://raw.githubusercontent.com/wicorn29/Chromeify-LIGHT/main/test.sh)
 
 
 function runCommand() { 
